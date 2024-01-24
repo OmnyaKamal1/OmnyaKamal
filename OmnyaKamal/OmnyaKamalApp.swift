4d96a4e92fe0ffd4180c47ed99196635a88bf714
omnya <omnyakamal1@icloud.com>
2024-01-24 20:51:24 +0300

    Initial Commit

---
+++ OmnyaKamal/OmnyaKamalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmnyaKamalApp.swift
+//  OmnyaKamal
+//
+//  Created by Omnya Kamal  on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct OmnyaKamalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
